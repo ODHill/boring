70f2d5239106ef42093b64e67d787c7a458bcd56
Oscar Hill <danielh_1982@hotmail.com>
2020-04-05 23:54:40 -0300

    First commit

---
+++ src/test/java/com/odhill/boring/BoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.odhill.boring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
